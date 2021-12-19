00ece343ac712443f5ea1f9bdf9b46343f474a29
stupidguy176 <hoaian176>
2021-12-19 20:09:41 +0700

    input to json

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 https://viblo.asia/p/crawl-lyric-100-bai-hat-hot-nhat-zing-mp3-voi-nodejs-va-puppeteer-924lJA2WZPM#_cai-dat-moi-truong-1
 https://pptr.dev/
+
+## Step to run
+
+npm install puppeteer
+
+node index
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+const puppeteer = require('puppeteer')
+const fs = require('fs')
 
-(async () => {
+;(async () => {
   // const browser = await puppeteer.launch({ headless: false })
   const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
 
-  await page.goto('https://www.nhaccuatui.com/playlist/acoustic-viet-gay-nghien-va.b7dMcA0UhTPw.html')
+  await page.goto(
+    'https://www.nhaccuatui.com/playlist/acoustic-viet-gay-nghien-va.b7dMcA0UhTPw.html'
+  )
   const songs = await page.evaluate(() => {
     let songs = document.getElementsByClassName('item_content')
     songs = [...songs]
     let array = songs.map((song) => {
       return {
-        title: song
-          .getElementsByClassName('name_song')[0]
-          .innerHTML.trim(),
+        title: song.getElementsByClassName('name_song')[0].innerHTML.trim(),
         url: song.getElementsByClassName('name_song')[0].href,
       }
     })
@@ -25,11 +25,10 @@ const fs = require('fs');
     for (let song of songs) {
       await page.goto(song.url)
       let lyric = await page.evaluate(() => {
-        let ly = document
-        .getElementById('divLyric'),
-        lyric = ''
+        let ly = document.getElementById('divLyric'),
+          lyric = ''
 
-        if(ly) {
+        if (ly) {
           lyric = document
             .getElementById('divLyric')
             .innerHTML.replace(/\<br\>/g, '')
@@ -37,13 +36,38 @@ const fs = require('fs');
 
         return lyric
       })
-      await fs.writeFile(`songs/${song.title}.txt`, lyric, function (err) {
-        if (err) throw err
-        console.log('Đã lưu:' + song.title)
+
+      // save data
+      // await fs.writeFile(`songs/${song.title}.txt`, lyric, function (err) {
+      //   if (err) throw err
+      //   console.log('Đã lưu:' + song.title)
+      // })
+      // add data to json file
+      // https://stackoverflow.com/questions/36856232/write-add-data-in-json-file-using-node-js
+      let obj = {
+        songs: [],
+      }
+      fs.readFile('songs.json', function readFileCallback(err, data) {
+        if (err) {
+          console.log(err)
+        } else {
+          obj = JSON.parse(data)
+
+          for (let i = 0; i < 5; i++) {
+            obj.songs.push({
+              title: song.title,
+              lyric: (lyric || '').slice(0, 400),
+              url: song.url || ''
+            })
+          }
+
+          let json = JSON.stringify(obj)
+          fs.writeFile('songs.json', json, function(err) {
+            if (err) throw err
+          })
+        }
       })
     }
   }
   await browser.close()
-
 })()
-
